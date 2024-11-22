82a77891861f0a25087d828f3b18235dbe7be3f5
simon <simonescuela9@gmail.com>
2024-11-22 09:40:57 +0000

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Surface {
+        }
+    }
+}
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+}
